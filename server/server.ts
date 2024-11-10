b5fcab04b377543bcbb23f601fb4bd7fad31983d
Ashlin Lee <ashlinlee98@gmail.com>
2024-11-09 21:43:12 -0700

    Fixed express version issue

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,15 @@
-import express from 'express';
+import express, { Application } from 'express';
 import { createServer } from 'http';
 import { Server, Socket } from 'socket.io';
+import { ApolloServer } from 'apollo-server-express';
+import connection from './config/connection';
+import { verifyToken } from './utils/jwt';
 
-const app = express();
+//import the next line after creating the schema
+//import schema from './schema';
+
+const app: Application = express();
+connection();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
@@ -19,6 +26,29 @@ io.on('connection', (socket: Socket) => {
     });
 });
 
-httpServer.listen(3001, () => {
-    console.log('Server is running on port 3001');
+const apolloServer = new ApolloServer({
+  schema,
+  context: ({ req }) => {
+    const token = req.headers.authorization || '';
+    try {
+      const payload = verifyToken(token);
+      const userId = payload.userId;
+      console.log('User ID:', userId);
+      return { userId };
+    } catch (error) {
+      console.error('Error:', error);
+      return error;
+    }
+  },
+});
+
+await apolloServer.start();
+
+apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+const PORT = process.env.PORT || 3001;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL server running on http://localhost:${PORT}${apolloServer.graphqlPath}`);
 });
