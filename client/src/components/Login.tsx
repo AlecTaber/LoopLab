2d5bd0da3532484785894cf452d889f2bcc83f09
Alec Taber <alectaber12@gmail.com>
2024-11-11 15:37:43 -0800

    add Login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -14,4 +14,25 @@ interface LoginProps {
     errors?: Array<{message: string}>;
 }
 
-const Login: React.FC =
+const Login: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [message, setMessage] = useState<string | null>(null);
+    const [isError, setIsError] = useState<boolean>(false);
+
+    const GRAPHQL_URL = 'http://localhost:3001/graphql';
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const query = `
+            mutation Login($email: String!, $password: String!) {  
+                login(email: $email, password: $password) {
+                    token
+                    user {
+                        id
+                        username
+                        email
+                    }
+                }
+            }
+        `;
