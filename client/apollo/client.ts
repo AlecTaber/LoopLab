8aabd5c5b2e7e846462fbf498b714069a99f1d25
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-11-25 18:38:38 -0800

    added back end url property into the front end

--- client/apollo/client.ts
+++ client/apollo/client.ts
@@ -1,8 +1,13 @@
 import { ApolloClient } from '@apollo/client';
 import cache from './cache';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const backEndUrl = process.env.RENDER_BACKEND_URL
 
 const client = new ApolloClient({
-    uri: 'http://localhost:3001/graphql',
+    uri: backEndUrl || 'http://localhost:3001/graphql',
     cache,
 });
 
--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,6 +1,7 @@
 import { io, Socket } from 'socket.io-client';
+const backEndUrl = import.meta.env.RENDER_BACKEND_URL
 
-const socket: Socket = io(process.env.PORT || "http://localhost:3001", {
+const socket: Socket = io(backEndUrl || "http://localhost:3001", {
     autoConnect: true,
 });
 
