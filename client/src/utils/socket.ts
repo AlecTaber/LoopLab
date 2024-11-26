2b217d446002c41418c7fc1589a02a9fa566c561
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-11-25 18:40:29 -0800

    added process.env to socket.ts

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,5 +1,8 @@
 import { io, Socket } from 'socket.io-client';
-const backEndUrl = import.meta.env.RENDER_BACKEND_URL
+import dotenv from 'dotenv';
+
+dotenv.config()
+const backEndUrl = process.env.RENDER_BACKEND_URL
 
 const socket: Socket = io(backEndUrl || "http://localhost:3001", {
     autoConnect: true,
